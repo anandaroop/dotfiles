4e8f293fdd1717ca21a34bbdbd7e51df6b6405f7
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-03-21 23:33:58 -0400

    Colorize

--- zshrc.sh
+++ zshrc.sh
@@ -7,6 +7,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# colorize terminal output
+export CLICOLOR=1
+
 # source dotfile aliases
 source ~/src/dotfiles/aliases.sh
 source ~/src/dotfiles/aliases-artsy.sh
