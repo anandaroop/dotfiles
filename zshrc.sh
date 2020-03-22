beab04d7495f315fa84c20b5853f3773dcb18c95
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-03-22 01:43:13 -0400

    Enable starship prompt

--- zshrc.sh
+++ zshrc.sh
@@ -7,6 +7,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# enable starship (after brew install starship)
+eval "$(starship init zsh)"
+
 # colorize terminal output
 export CLICOLOR=1
 
