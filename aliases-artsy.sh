b124a1d33e2a4c6d840f8e5db282dba71845c8c8
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2021-11-18 12:54:23 -0500

    Steal Joey's k8s shortcuts

--- aliases-artsy.sh
+++ aliases-artsy.sh
@@ -1,7 +1,9 @@
 # see aliases.sh for ruby/js workflow stuff
 
 # k8s
-alias k8s-token="aws-iam-authenticator token -i $(kubectl config get-clusters | grep kubernetes-production) -r arn:aws:iam::585031190124:role/KubernetesDev | jq -r '.status.token'"
+# alias k8s-token="aws-iam-authenticator token -i $(kubectl config get-clusters | grep kubernetes-production) -r arn:aws:iam::585031190124:role/KubernetesDev | jq -r '.status.token'"
+alias k8s="aws-iam-authenticator token -i $(kubectl config get-clusters | grep kubernetes-staging) -r arn:aws:iam::585031190124:role/KubernetesDev | jq -r .status.token | pbcopy && echo \"copied to clipboard\""
+alias k8p="aws-iam-authenticator token -i $(kubectl config get-clusters | grep kubernetes-production) -r arn:aws:iam::585031190124:role/KubernetesDev | jq -r .status.token | pbcopy && echo \"copied to clipboard\""
 
 # hokusai
 alias hse="hokusai staging env get"
