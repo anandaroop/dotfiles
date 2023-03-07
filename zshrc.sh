242c98d6fb06a61a3ebd189b2948d58073d12a47
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2023-03-07 13:41:46 -0500

    chore: update android config

--- zshrc.sh
+++ zshrc.sh
@@ -46,8 +46,11 @@ source ~/src/dotfiles/local.sh
 export PATH=~/src/dotfiles/bin:~/bin:$PATH
 
 # android dev
-export ANDROID_HOME=$HOME/Library/Android/sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/tools/bin
-export PATH=$PATH:$ANDROID_HOME/platform-tools
+# export ANDROID_HOME=$HOME/Library/Android/sdk
+# export PATH=$PATH:$ANDROID_HOME/emulator
+# export PATH=$PATH:$ANDROID_HOME/tools
+# export PATH=$PATH:$ANDROID_HOME/tools/bin
+# export PATH=$PATH:$ANDROID_HOME/platform-tools
+export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_SDK_ROOT/emulator
+export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
