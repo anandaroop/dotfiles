26bf95fc1b1f21d635cd0012ec8474d452f2873f
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2025-06-27 22:33:49 -0400

    feat: userid

--- aliases-artsy.sh
+++ aliases-artsy.sh
@@ -85,3 +85,4 @@ alias es6up="docker run -it -p 9200:9200 -e http.cors.enabled=true -e http.cors.
 alias es7up="docker run -it -p 9200:9200 -e http.cors.enabled=true -e http.cors.allow-origin='*' -e \"discovery.type=single-node\" elasticsearch:7.17.12"
 
 alias observe="cd ~/src/me/observe-artsy && code . && yarn dev && op signin"
+alias userid="op item get 'Artsy (old gmail login)' --fields 'username,Gravity User ID' && op item get 'Artsy (roop@)' --fields 'username,Gravity User ID'"
