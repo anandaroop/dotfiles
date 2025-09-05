d5f352b5f2f2bc0c89036f340344a4d783eea6ba
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2025-09-05 17:54:51 -0400

    feat: add persistent volume to es7up

--- aliases-artsy.sh
+++ aliases-artsy.sh
@@ -82,7 +82,7 @@ alias onyxe2e="cd ~/src/artsy/integrity && git pull artsy main && yarn cypress r
 
 # post m1
 alias es6up="docker run -it -p 9200:9200 -e http.cors.enabled=true -e http.cors.allow-origin='*' -e \"discovery.type=single-node\" --platform linux/amd64 elasticsearch:6.8.23"
-alias es7up="docker run -it -p 9200:9200 -e http.cors.enabled=true -e http.cors.allow-origin='*' -e \"discovery.type=single-node\" elasticsearch:7.17.12"
+alias es7up="docker run -it -p 9200:9200 -v elasticsearch_data:/usr/share/elasticsearch/data -e http.cors.enabled=true -e http.cors.allow-origin='*' -e 'discovery.type=single-node' -e 'xpack.security.enabled=false' elasticsearch:7.17.12"
 
 alias observe="cd ~/src/me/observe-artsy && code . && yarn dev && op signin"
 alias userid="op item get 'Artsy (old gmail login)' --fields 'username,Gravity User ID' && op item get 'Artsy (roop@)' --fields 'username,Gravity User ID'"
